07269d34dd797386a65452d8a21166736f974f9e
kevin <hyunbin7303@gmail.com>
2023-09-09 22:44:19 -0400

    code update for tinting.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,6 +6,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to wait for database."""
 
@@ -15,11 +16,10 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
-                self.check(databases= ['default'])
+                self.check(databases=['default'])
                 db_up = True
-            except(Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 seconds....')
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 seconds.')
                 time.sleep(1)
-
 
         self.stdout.write(self.style.SUCCESS('Database available.'))
